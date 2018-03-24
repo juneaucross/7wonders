18be81cc506410e349f1e35229c609426602aa5b
Peter Cross <itextx@gmail.com>
2018-03-24 09:32:31 +0300

    tabindex fix

--- src/App.vue
+++ src/App.vue
@@ -15,31 +15,31 @@
         </th>
         <tr class="table__player-row" v-for="(player, i) in playersArr">
           <td class="table__player table__player--name">
-            <input type="text" :name="'playerName-' + (i+1)" v-model="player.name" value="">
+            <input type="text" :name="'playerName-' + (i+1)" v-model="player.name" value="" tabindex="1">
           </td>
           <td class="table__player table__player--red">
-            <input type="number" :name="'red-' + (i+1)" value="" v-model="player.red">
+            <input type="number" :name="'red-' + (i+1)" value="" v-model="player.red" tabindex="2">
           </td>
           <td class="table__player table__player--gold">
-            <input type="number" :name="'gold-' + (i+1)" value="" v-model="player.gold">
+            <input type="number" :name="'gold-' + (i+1)" value="" v-model="player.gold" tabindex="3">
           </td>
           <td class="table__player table__player--wonder">
-            <input type="number" :name="'wonder-' + (i+1)" value="" v-model="player.wonder">
+            <input type="number" :name="'wonder-' + (i+1)" value="" v-model="player.wonder" tabindex="4">
           </td>
           <td class="table__player table__player--blue">
-            <input type="number" :name="'blue-' + (i+1)" value="" v-model="player.blue">
+            <input type="number" :name="'blue-' + (i+1)" value="" v-model="player.blue" tabindex="5">
           </td>
           <td class="table__player table__player--orange">
-            <input type="number" :name="'orange-' + (i+1)" value="" v-model="player.orange">
+            <input type="number" :name="'orange-' + (i+1)" value="" v-model="player.orange" tabindex="6">
           </td>
           <td class="table__player table__player--violet">
-            <input type="number" :name="'violet-' + (i+1)" value="" v-model="player.violet">
+            <input type="number" :name="'violet-' + (i+1)" value="" v-model="player.violet" tabindex="7">
           </td>
           <td class="table__player table__player--green">
-            <input type="number" :name="'green-' + (i+1)" value="" v-model="player.green">
+            <input type="number" :name="'green-' + (i+1)" value="" v-model="player.green" tabindex="8">
           </td>
           <td class="table__player table__player--leader">
-            <input type="number" :name="'leader-' + (i+1)" value="" v-model="player.leader">
+            <input type="number" :name="'leader-' + (i+1)" value="" v-model="player.leader" tabindex="9">
           </td>
         </tr>
       </table>
