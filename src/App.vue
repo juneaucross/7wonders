98b839b52b5714aa1fa234a7172d9ea01214418a
Peter Cross <itextx@gmail.com>
2019-07-02 19:42:56 +0300

    styles added

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,72 @@
 <template>
   <div id="app">
-    <form class="main" action="" method="post">
-      <table class="table">
-        <th class="table__head-row">
-          <td class="table__head table__head--name">player name</td>
-          <td class="table__head table__head--red">war</td>
-          <td class="table__head table__head--gold">gold</td>
-          <td class="table__head table__head--wonder">wonder</td>
-          <td class="table__head table__head--blue">culture</td>
-          <td class="table__head table__head--orange">market</td>
-          <td class="table__head table__head--violet">guild</td>
-          <td class="table__head table__head--green">science</td>
-          <td class="table__head table__head--leader">leader</td>
-        </th>
-        <tr class="table__player-row" v-for="(player, i) in playersArr">
-          <td class="table__player table__player--name">
-            <input type="text" :name="'playerName-' + (i+1)" v-model="player.name" value="" tabindex="1">
-          </td>
-          <td class="table__player table__player--red">
-            <input type="number" :name="'red-' + (i+1)" value="" v-model="player.red" tabindex="2">
-          </td>
-          <td class="table__player table__player--gold">
-            <input type="number" :name="'gold-' + (i+1)" value="" v-model="player.gold" tabindex="3">
-          </td>
-          <td class="table__player table__player--wonder">
-            <input type="number" :name="'wonder-' + (i+1)" value="" v-model="player.wonder" tabindex="4">
-          </td>
-          <td class="table__player table__player--blue">
-            <input type="number" :name="'blue-' + (i+1)" value="" v-model="player.blue" tabindex="5">
-          </td>
-          <td class="table__player table__player--orange">
-            <input type="number" :name="'orange-' + (i+1)" value="" v-model="player.orange" tabindex="6">
-          </td>
-          <td class="table__player table__player--violet">
-            <input type="number" :name="'violet-' + (i+1)" value="" v-model="player.violet" tabindex="7">
-          </td>
-          <td class="table__player table__player--green">
-            <input type="number" :name="'green-' + (i+1)" value="" v-model="player.green" tabindex="8">
-          </td>
-          <td class="table__player table__player--leader">
-            <input type="number" :name="'leader-' + (i+1)" value="" v-model="player.leader" tabindex="9">
-          </td>
-        </tr>
-      </table>
-      <!-- <button @click.prevent="submitForm">Submit scores</button> -->
-    </form>
-    <button id="addPlayerBtn"  @click="addPlayer">add player</button>
-    <button @click="getSum">calculate scores</button>
+    <div class="container">
 
-    <div class="result__wrap" v-if="sortedArr">
-      <ul class="result__list">
-        <li class="result__list-item" v-for="player in sortedArr">
-          {{ player.name | capitalize }} - {{ player.sum }} !
-        </li>
-      </ul>
+      <div class="logo">
+        <h1 class="logo-heading"><span class="logo-number">7</span>wonders</h1>
+      </div>
+      <form class="main" action="" method="post">
+        <table class="table">
+          <th class="table__head-row">
+            <td class="table__head table__head--name">player name</td>
+            <td class="table__head table__head--red">war</td>
+            <td class="table__head table__head--gold">gold</td>
+            <td class="table__head table__head--wonder">wonder</td>
+            <td class="table__head table__head--blue">culture</td>
+            <td class="table__head table__head--orange">market</td>
+            <td class="table__head table__head--violet">guild</td>
+            <td class="table__head table__head--green">science</td>
+            <td class="table__head table__head--leader">leader</td>
+          </th>
+          <transition-group name="list" appear>
+            <tr class="table__player-row" v-for="(player, i) in playersArr" :key="i">
+              <td class="table__player table__player--name">
+                <input type="text" :name="'playerName-' + (i+1)" v-model="player.name" value="" tabindex="1" placeholder=" ">
+              </td>
+              <td class="table__player table__player--red">
+                <input type="number" :name="'red-' + (i+1)" value="" v-model="player.red" tabindex="2" placeholder=" ">
+              </td>
+              <td class="table__player table__player--gold">
+                <input type="number" :name="'gold-' + (i+1)" value="" v-model="player.gold" tabindex="3" placeholder=" ">
+              </td>
+              <td class="table__player table__player--wonder">
+                <input type="number" :name="'wonder-' + (i+1)" value="" v-model="player.wonder" tabindex="4" placeholder=" ">
+              </td>
+              <td class="table__player table__player--blue">
+                <input type="number" :name="'blue-' + (i+1)" value="" v-model="player.blue" tabindex="5" placeholder=" ">
+              </td>
+              <td class="table__player table__player--orange">
+                <input type="number" :name="'orange-' + (i+1)" value="" v-model="player.orange" tabindex="6" placeholder=" ">
+              </td>
+              <td class="table__player table__player--violet">
+                <input type="number" :name="'violet-' + (i+1)" value="" v-model="player.violet" tabindex="7" placeholder=" ">
+              </td>
+              <td class="table__player table__player--green">
+                <input type="number" :name="'green-' + (i+1)" value="" v-model="player.green" tabindex="8" placeholder=" ">
+              </td>
+              <td class="table__player table__player--leader">
+                <input type="number" :name="'leader-' + (i+1)" value="" v-model="player.leader" tabindex="9" placeholder=" ">
+              </td>
+            </tr>
+          </transition-group>
+
+        </table>
+        <!-- <button @click.prevent="submitForm">Submit scores</button> -->
+      </form>
+      <div class="buttons-wrap">
+        <button id="addPlayerBtn"  @click="addPlayer">add</button>
+        <button id="getSumBtn" @click="getSum">scores</button>
+      </div>
+
+      <div class="result__wrap" v-if="sortedArr">
+        <ul class="result__list">
+          <li class="result__list-item" v-for="player in sortedArr">
+            {{ player.name | capitalize }} - {{ player.sum }} !
+          </li>
+        </ul>
+      </div>
+
+
     </div>
 
 
@@ -127,15 +140,48 @@ export default {
 </script>
 
 <style>
+
   #app {
+    /* css variables */
+    --bgColor: #2E364A;
+    --inputFocusColor: #232834;
+    --inputPlaceholderColor: #030814;
+
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    font-weight: 300;
     text-align: center;
     color: white;
     width: 100%;
-    background: black;
-    margin-top: 60px;
+    background: var(--bgColor);
+    /* margin-top: 60px; */
+  }
+
+  .container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 1280px;
+    margin: 0 auto;
+  }
+
+  .logo-heading {
+    width: 100%;
+    position: relative;
+    text-transform: uppercase;
+    font-size: 25px;
+    line-height: 1.4;
+  }
+
+  .logo-number {
+    display: block;
+    position: absolute;
+    top: -10px;
+    left: -20px;
+    font-size: 38px;
+    line-height: 1.4;
+    font-weight: 500;
   }
 
   .table {
@@ -148,16 +194,34 @@ export default {
   .table__player-row {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    width: 1190px;
+    height: 64px;
+    justify-content: space-around;
+    -ms-align-items: center;
+    align-items: center;
     text-align: center;
   }
 
+  .table__player-row {
+    border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  }
+
+  .table__player-row:nth-child(odd) {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
   .table__head {
-    width: 80px;
+    width: 86px;
+    font-size: 20px;
+    line-height: 1.2;
   }
 
   .table__player {
-    width: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 86px;
+    height: 100%;
   }
 
   .table__player--name,
@@ -176,11 +240,91 @@ export default {
   }
 
   input {
-    width: 50%;
+    width: 80%;
+    height: 24px;
+    color: #fff;
+    font-size: 20px;
+    line-height: 1.2;
+    background-color: var(--bgColor);
+    border: none;
+  }
+
+  input:placeholder-shown {
+    background-color: var(--inputPlaceholderColor);
+  }
+
+  input:focus {
+    width: 100%;
+    height: 100%;
+    font-size: 35px;
+    line-height: 1.2;
+    background-color: var(--inputFocusColor);
+    outline: none;
   }
 
   input::-webkit-inner-spin-button {
-  display: none;
-}
+    display: none;
+  }
+
+  .table__player-row:nth-child(odd) > input {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
+  .buttons-wrap {
+    display: flex;
+    justify-content: flex-end;
+    margin: 45px 0;
+    width: 100%;
+  }
+
+  button {
+    width: 140px;
+    height: 43px;
+    margin: 0 20px;
+    border-radius: 50px;
+    font-size: 20px;
+    line-height: 1.2;
+    color: #fff;
+    background-color: transparent;
+    border: 2px solid #fff;
+    transition: all 0.2s linear;
+    outline: none;
+  }
+
+  button:hover {
+    color: var(--bgColor);
+    background-color: #fff;
+    /* border: 2px solid #fff; */
+  }
+
+  button:disabled {
+    color: rgba(255, 255, 255, 0.3);
+    border: 2px solid rgba(255, 255, 255, 0.3);
+  }
+
+
+
+
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active до версии 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+
+
+
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(30px);
+  }
 
 </style>
